6aa9ff3f1b056af1ced316860be6ffd522cf89a4
Heta Dave <heta@outlook.com>
2024-02-23 19:38:33 -0800

    Add boundary test cases + favicon

--- ship-game-tests.js
+++ ship-game-tests.js
@@ -1,9 +1,10 @@
-let ship1, ship2, currentBoard;
+let ship1, ship2, regularBoard, boundaryTestBoard;
 
+// Initialize the regular game board
 function initializeGameState() {
-  ship1 = { shipid: 1, Hits: 0, size: 2 };
-  ship2 = { shipid: 2, Hits: 0, size: 3 };
-  currentBoard = [
+  ship1 = { shipid: 1, hits: 0, size: 2 };
+  ship2 = { shipid: 2, hits: 0, size: 3 };
+  regularBoard = [
     { row: 3, col: 1, isHit: false, ship: ship2 },
     { row: 3, col: 2, isHit: false, ship: ship2 },
     { row: 3, col: 3, isHit: false, ship: ship2 },
@@ -12,42 +13,58 @@ function initializeGameState() {
   ];
 }
 
+// Initialize the boundary test board
+function initializeBoundaryTestBoard() {
+  ship1 = { shipid: 1, hits: 0, size: 2 };
+  ship2 = { shipid: 2, hits: 0, size: 3 };
+
+  // Creating a 5x5 board with ships on the boundaries
+  boundaryTestBoard = [
+    { row: 1, col: 1, isHit: false, ship: ship1 },
+    { row: 1, col: 2, isHit: false, ship: ship1 },
+    { row: 5, col: 3, isHit: false, ship: ship2 },
+    { row: 5, col: 4, isHit: false, ship: ship2 },
+    { row: 5, col: 5, isHit: false, ship: ship2 },
+  ];
+}
+
 // Function to check if a ship is hit, sunk, or missed
-function check(location) {
+function check(board, location) {
   // check for invalid input (i.e. row or col missing or not a number)
-  if (
-    !location ||
-    !location.row ||
-    !location.col ||
-    isNaN(location.row) ||
-    isNaN(location.col)
-  ) {
+  if (!location || !location.row || !location.col) {
     return "Invalid input";
   }
 
   const row = location.row;
   const col = location.col;
 
-  // check for out of bounds (assuming grid is 10x10)
-  if (row < 1 || row > 10 || col < 1 || col > 10) {
+  // check for out of bounds (for testing we are using 5x5 grid)
+  if (
+    !Number.isInteger(location.row) ||
+    !Number.isInteger(location.col) ||
+    row < 1 ||
+    row > 5 ||
+    col < 1 ||
+    col > 5
+  ) {
     return "Invalid input";
   }
 
-  for (let i = 0; i < currentBoard.length; i++) {
-    if (currentBoard[i].row === row && currentBoard[i].col === col) {
+  for (let i = 0; i < board.length; i++) {
+    if (board[i].row === row && board[i].col === col) {
       // Check if the ship is already sunk
-      if (currentBoard[i].ship.Hits === currentBoard[i].ship.size) {
+      if (board[i].ship.hits === board[i].ship.size) {
         return "Already sunk";
       }
       // Check if this is a Duplicate hit
-      if (currentBoard[i].isHit) {
+      if (board[i].isHit) {
         return "Duplicate hit";
       }
       // Else Increment the Hit count and register the hit for duplicate detection
-      currentBoard[i].isHit = true;
-      currentBoard[i].ship.Hits += 1;
+      board[i].isHit = true;
+      board[i].ship.hits += 1;
       // If Hit count equals its size, mark ship as sunk otherwise it's a Hit
-      if (currentBoard[i].ship.Hits === currentBoard[i].ship.size) {
+      if (board[i].ship.hits === board[i].ship.size) {
         return "Sink";
       } else {
         return "Hit";
@@ -59,7 +76,7 @@ function check(location) {
 }
 
 // Create a visual representation of the board, displaying ship locations, hits, and misses
-function renderBoard() {
+function renderBoard(board) {
   let boardHTML = "";
 
   for (let row = 1; row <= 5; row++) {
@@ -67,10 +84,7 @@ function renderBoard() {
       const cell = document.createElement("div");
       cell.classList.add("board-cell");
 
-      // Check if the cell is part of the currentBoard
-      const boardCell = currentBoard.find(
-        (c) => c.row === row && c.col === col
-      );
+      const boardCell = board.find((c) => c.row === row && c.col === col);
       if (boardCell) {
         cell.classList.add(boardCell.isHit ? "Hit" : "ship");
       } else {
@@ -79,7 +93,8 @@ function renderBoard() {
 
       boardHTML += cell.outerHTML;
     }
-    boardHTML += "<br>"; // Break line after each row
+    // Add a line break after each row
+    boardHTML += "<br>";
   }
 
   return boardHTML;
@@ -89,6 +104,8 @@ function renderBoard() {
 const testGroups = [
   {
     title: "Basic Game Actions",
+    getBoard: () => regularBoard,
+    boardSetup: initializeGameState,
     tests: [
       {
         title: "Check for Miss",
@@ -109,6 +126,8 @@ const testGroups = [
   },
   {
     title: "Duplicate hits and Sinking",
+    getBoard: () => regularBoard,
+    boardSetup: initializeGameState,
     tests: [
       {
         title: "Check for Hit",
@@ -138,7 +157,56 @@ const testGroups = [
     ],
   },
   {
+    title: "Boundary on 5x5 Grid",
+    getBoard: () => boundaryTestBoard,
+    boardSetup: initializeBoundaryTestBoard,
+    tests: [
+      {
+        title: "Top-left corner hit",
+        location: { row: 1, col: 1 },
+        expectedOutput: "Hit",
+      },
+      {
+        title: "Top-right corner hit",
+        location: { row: 1, col: 5 },
+        expectedOutput: "Miss",
+      },
+      {
+        title: "Bottom-right corner hit",
+        location: { row: 5, col: 5 },
+        expectedOutput: "Hit",
+      },
+      {
+        title: "Bottom-left corner hit",
+        location: { row: 5, col: 1 },
+        expectedOutput: "Miss",
+      },
+      {
+        title: "Middle edge top",
+        location: { row: 1, col: 2 },
+        expectedOutput: "Sink",
+      },
+      {
+        title: "Middle edge bottom",
+        location: { row: 5, col: 3 },
+        expectedOutput: "Hit",
+      },
+      {
+        title: "Middle edge left",
+        location: { row: 3, col: 1 },
+        expectedOutput: "Miss",
+      },
+      {
+        title: "Middle edge right",
+        location: { row: 3, col: 5 },
+        expectedOutput: "Miss",
+      },
+    ],
+  },
+  {
     title: "Invalid Inputs",
+    getBoard: () => regularBoard,
+    boardSetup: initializeGameState,
     tests: [
       {
         title: "Negative row",
@@ -152,12 +220,12 @@ const testGroups = [
       },
       {
         title: "Row out of bounds",
-        location: { row: 11, col: 5 },
+        location: { row: 6, col: 5 },
         expectedOutput: "Invalid input",
       },
       {
         title: "Column out of bounds",
-        location: { row: 5, col: 11 },
+        location: { row: 5, col: 6 },
         expectedOutput: "Invalid input",
       },
       {
@@ -185,8 +253,8 @@ const testGroups = [
 ];
 
 // Function to run a single test case
-function runTest(testCase) {
-  const output = check(testCase.location);
+function runTest(testCase, board) {
+  const output = check(board, testCase.location);
   const result = output === testCase.expectedOutput ? "Pass" : "Fail";
   return { output, result };
 }
@@ -196,7 +264,10 @@ const testCasesElement = document.getElementById("test-cases");
 
 // Iterate through each test group
 testGroups.forEach((group) => {
-  initializeGameState(); // Reset the game state at the beginning of each group
+  // Reset the game state at the beginning of each group
+  group.boardSetup();
+  const board = group.getBoard();
+  console.log(board);
 
   const groupElement = document.createElement("div");
   groupElement.classList.add("test-group");
@@ -204,13 +275,13 @@ testGroups.forEach((group) => {
 
   group.tests.forEach((testCase) => {
     // Render the board state before the test case
-    const boardBefore = renderBoard();
+    const boardBefore = renderBoard(board);
 
     // Run the test case
-    const { output, result } = runTest(testCase);
+    const { output, result } = runTest(testCase, board);
 
     // Render the board state after the test case
-    const boardAfter = renderBoard();
+    const boardAfter = renderBoard(board);
 
     // Create and append the test case element
     const testCaseElement = document.createElement("div");
